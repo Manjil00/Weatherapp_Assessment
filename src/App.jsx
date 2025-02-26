8f6a0b60dd09790bf731e70ef3f0b4c546852212
ACER <210220@softwarica.edu.np>
2025-02-26 15:33:35 +0545

    setup completetailwind v4

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+return (
+<div className="maincontainer h-[100vh] w-full bg-green-800">
+    <h1 className="text-red-800 text-center text-6xl">Setupp Weatherapp</h1>
+</div>
+  )
+}
+
+export default App;
